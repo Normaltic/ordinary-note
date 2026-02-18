7ba638b8d0decd88bf6947a140bd26a5b49e9848
Yunji Kim <normaltic@gmail.com>
2026-02-18 13:54:01 +0900

    chore: TypeScript, Prettier, ESLint 공통 설정 추가

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/.turbo/**'],
+  },
+);
