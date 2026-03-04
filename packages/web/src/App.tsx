62fe00a78c394280449e8e2adcaff1938426fe43
Yunji Kim <normaltic@gmail.com>
2026-03-04 21:48:43 +0900

    fix: 존재하지 않는 경로 접근 시 루트로 리다이렉트

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/query-client';
 import { AppShell } from './pages/_layout';
@@ -35,7 +35,9 @@ export function App() {
           <Route index element={<IndexPage />} />
           <Route path="folders/:folderId" element={<FolderPage />} />
           <Route path="notes/:noteId" element={<NotePage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       </GlobalErrorHandler>
     </BrowserRouter>
